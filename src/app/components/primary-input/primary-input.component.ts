31b8f0e1f74772dbf3bc2a20e4bdd798763c9799
Karoline Vieira Neves <karolconkah1@outlook.com>
2026-02-19 13:45:10 -0300

    updates

--- src/app/components/primary-input/primary-input.component.ts
+++ src/app/components/primary-input/primary-input.component.ts
@@ -8,11 +8,44 @@ type InputTypes = "text" | "email" | "password"
   selector: 'app-primary-input',
   standalone: true,
   imports: [ ReactiveFormsModule],
+  providers: [
+     {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => PrimaryInputComponent),
+      multi: true
+     }
+  ],
   templateUrl: './primary-input.component.html',
   styleUrl: './primary-input.component.scss'
 })
-export class PrimaryInputComponent {
+export class PrimaryInputComponent implements ControlValueAccessor {
 @Input() type: InputTypes = "text";
-@Input() formName: string = "";
 @Input() placeholder: string = "";
+@Input() label: string = "";
+@Input() inputName: string = "";
+
+value: string = '';
+
+onChange: any = () => {};
+onTouched: any = () => {};
+
+onInput(event: Event) {
+  const value = (event.target as HTMLInputElement).value;
+  this.onChange(value);
+}
+
+writeValue(value: any): void {
+  this.value = value;
+}
+
+registerOnChange(fn: any): void {
+  this.onChange = fn;
+}
+
+registerOnTouched(fn: any): void {
+  this.onTouched = fn;
+}
+
+setDisabledState(isDisabled: boolean): void {}
+
 }
